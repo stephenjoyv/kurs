c29826bffe3ed25ea23d6d822eac4da7f960a10b
Stephen Joyv <stepisraelyan@gmail.com>
2024-10-06 06:38:13 +0300

    Release version 1.2.

---
+++ src/main/java/com/kurs/kurs/Animal.java
@@ -0,0 +1,70 @@
+package com.kurs.kurs;
+
+import java.util.ArrayList;
+
+public class Animal {
+    private String name;
+    private String owner;
+    private int price;
+    private int age;
+    private String bornDate;
+    private String description;
+
+    public ArrayList<String> tags;
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBornDate() {
+        return bornDate;
+    }
+
+    public void setBornDate(String bornDate) {
+        this.bornDate = bornDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
